466251370d732b60e41c196413a591fedb32c7cb
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2023-05-02 21:00:25 -0300

    estructura de carpetas

---
+++ arduino/arduino.ino
@@ -0,0 +1,23 @@
+//Prender luz
+
+int led = 13;
+
+void setup(){
+   pinMode(led, OUTPUT);
+}
+
+void loop(){
+   digitalWrite(led.HIGH);
+}
+
+//Apagar Luz
+
+int led = 13;
+
+void setup(){
+   pinMode(led, OUTPUT);
+}
+
+void loop(){
+   digitalWrite(led.LOW);
+}
